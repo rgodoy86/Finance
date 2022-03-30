f2a4cc7857f669c4467822fbe341a10ca34e4091
rgodoy86 <rgodoy86@gmail.com>
2022-03-30 11:21:16 +0200

    SMA and EMA created

--- indicators.py
+++ indicators.py
@@ -1,3 +1,14 @@
 class Indicators:
+
     def __init__():
         pass
+
+    ## Simple Media Average
+    def SMA(self, df, period=9):
+        df["SMA_" + str(period)] = df.loc[:,"Close"].rolling(window=period).mean()
+        return df
+
+    # Exponential Media Average
+    def EMA(self, df, period=9):
+        df["EMA_" + str(period)] = df.loc[:,"Close"].ewm(span=period, adjust=False).mean()
+        return df

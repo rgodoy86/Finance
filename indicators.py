cbc714f3bd1f7be01d0cb8aa0dba037c91b69fb3
Raphael Rebello de Godoy <57528077+rgodoy86@users.noreply.github.com>
2022-04-19 19:33:48 +0200

    Update indicators.py
    Added Aroon Up and Down indicator

--- indicators.py
+++ indicators.py
@@ -28,7 +28,42 @@ class Indicators:
   def get_indicators_df(self, ohlc_df):
     self._indicators_df = ohlc_df
     return self._indicators_df
-     
+   
+  ### Aroon
+  # https://www.investopedia.com/terms/a/aroon.asp
+  # Main: trend indicator
+  # Type: two line cross
+  # 
+  # Aroon Up is above the Aroon Down - bullish behavior (Up Trend)
+  # Aroon Down is above the Aroon Up - bearing behavior (Down Trend)
+  # Crossovers of the two lines can signal trend changes
+  #
+  def aroon(self, period=14, plot=False):
+    self._period = period
+    up_header = "Aroon Up"
+    down_header = "Aroon Down"
+    header = "Output Aroon"
+
+    self._indicators_df[up_header] = 100 * self._indicators_df.High.rolling(period + 1).apply(lambda x: x.argmax()) / period
+    self._indicators_df[down_header] = 100 * self._indicators_df.Low.rolling(period + 1).apply(lambda x: x.argmin()) / period
+
+    # Indicator Output
+    self._indicators_df.loc[self._indicators_df[up_header] > self._indicators_df[down_header], header] = True   # Up > Down
+    self._indicators_df.loc[self._indicators_df[up_header] <= self._indicators_df[down_header], header] = False 
+
+    if (plot is True):
+      #Create and plot the graph
+      plt.figure(figsize=(12.2,4.5))
+      plt.plot(self._indicators_df[up_header], color="lightblue",  label=up_header) 
+      plt.plot(self._indicators_df[down_header], color="tomato"  ,label=down_header) 
+
+      plt.title("Aroon Up and Down")
+      plt.legend(loc='upper left')
+      plt.show()
+
+    self._indicators_df.drop([up_header, down_header], axis=1, inplace=True)
+    return self._indicators_df
+  
   ### Exponential Moving Average
   def EMA(self, period=9):
 

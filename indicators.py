adfe300fa9ab9f902bda3a01eba8b093363ef663
Raphael Rebello de Godoy <57528077+rgodoy86@users.noreply.github.com>
2022-04-19 19:32:54 +0200

    Update indicators.py
    Added getters / setters
    Update EMA and SMA
    Update constructor

--- indicators.py
+++ indicators.py
@@ -1,14 +1,48 @@
+import plotly.graph_objects as go
+
 class Indicators:
 
-    def __init__(self):
-        pass
+  def __init__(self, ohlc_df):
+    self._indicators_df = ohlc_df
+    self._ohlcv_list = ['Open','High','Low','Close','Volume']
+    
+    #Create and plot the graph
 
-    ## Simple Media Average
-    def SMA(self, df, period=9):
-        df["SMA_" + str(period)] = df.loc[:,"Close"].rolling(window=period).mean()
-        return df
+    fig = go.Figure(data=[go.Candlestick(x=self._indicators_df.index,
+                                          open=self._indicators_df["Open"],
+                                          high=self._indicators_df["High"],
+                                          low=self._indicators_df["Low"],
+                                          close=self._indicators_df["Close"])
+    ])
+    
+    fig.update_layout(
+        title = "Candlestick", 
+        xaxis_title = "Date", 
+        yaxis_title = "Price")
+    fig.show()    
 
-    # Exponential Media Average
-    def EMA(self, df, period=9):
-        df["EMA_" + str(period)] = df.loc[:,"Close"].ewm(span=period, adjust=False).mean()
-        return df
+  ### Getters / Setters
+  def get_indicators_df(self):
+    return self._indicators_df
+
+  def get_indicators_df(self, ohlc_df):
+    self._indicators_df = ohlc_df
+    return self._indicators_df
+     
+  ### Exponential Moving Average
+  def EMA(self, period=9):
+
+    header = "Output EMA"
+    self._indicators_df["EMA"] = self._indicators_df.loc[:,"Close"].ewm(span=period,adjust=False).mean()
+    self._indicators_df[header] = self._indicators_df["Close"] > self._indicators_df["EMA"]
+
+    return self._indicators_df
+
+  ### Simple Moving Average
+  def SMA(self, period=9):
+
+    header = "Output SMA"
+    self._indicators_df["SMA"] = self._indicators_df.loc[:,"Close"].rolling(window=period).mean()
+    self._indicators_df[header] = self._indicators_df["Close"] > self._indicators_df["SMA"]
+
+    return self._indicators_df
